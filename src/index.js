1ca043169b3f7dba59fa586f2f2de382475d182b
Glauco Pater <glauco@monoqi.de>
2019-01-18 14:28:47 +0100

    Add Drad and Drop events

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom"; 
-import Board from "./containers/Board/Board"; 
-
+import Board from "./containers/Board/Board";  
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Kanban</h1> 
-      <Board />  
+      <Board />
     </div>
   );
 }
